6eb6a77ba8fc7c443095158474be0f48a76dd088
github username <jpsypek@gmail.com>
2019-07-11 14:34:21 -0600

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,42 @@ function printString(myString) {
   }
 }
 
-// function reverseString(string) {
-//   if (string.length < 2)
-//
-// }
+function reverseString(myString) {
+  if (myString.length < 2) {
+    return myString
+  } else {
+    return reverseString(myString.substring(1)) + myString[0]
+  }
+}
+
+function isPalindrome(myString) {
+  if (myString.length < 2) {
+    return true
+  } else if (myString[0] === myString[myString.length - 1]) {
+    return isPalindrome(myString.substring(1, myString.length -1))
+  } else {
+    return false
+  }
+}
+
+function addUpTo(myArray, index) {
+  return index ? myArray[index] + addUpTo(myArray, --index) : myArray[index]
+}
+
+function maxOf(myArray) {
+  if (myArray.length === 1) {
+    return myArray[0]
+  } else {
+    return Math.max(myArray.pop(), maxOf(myArray))
+  }
+}
+
+function includesNumber(myArray, myNumber) {
+  if (myArray[0] === myNumber) {
+    return true
+  } else if (myArray.length > 1) {
+    return includesNumber(myArray.slice(1), myNumber)
+  } else {
+    return false
+  }
+}
